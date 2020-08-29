a941f06ecedbc2a506d1a0dec2f7d8292af1a9f7
yar-ua <yar.ua.1985@gmail.com>
2020-08-29 10:08:17 +0300

    refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
 
       # Uncomment the section below if you want users to be created if they don't exist
       unless user
-          user = User.create(name: email: data['email'],
+          user = User.create(email: data['email'],
              password: Devise.friendly_token[0,20]
           )
       end
